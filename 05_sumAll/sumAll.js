c6a5fef015d78c1fb6eead82320832e11fbafe32
Liemmjo <140917436+Liemmjo24@users.noreply.github.com>
2023-12-20 22:34:57 -0500

    Exercise 5 Done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
 
+    let result = 0;
+    for(let x = min; x <= max; x++)
+    {
+        result += x;
+    }
+    return result;
 };
 
 // Do not edit below this line
